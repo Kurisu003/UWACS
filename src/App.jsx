fb549dbf1990fd7a1c3d7af8b6bea5534dce8983
Daniel <74920220+Kurisu003@users.noreply.github.com>
2025-09-06 13:10:27 +0200

    See description
    Fixed issues where only 1 would work, implemented console clear button

--- src/App.jsx
+++ src/App.jsx
@@ -3,58 +3,71 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-    const [pfp_hid_status, set_pfp_hid_status] = useState(false);
-    const [ufc_hid_status, set_ufc_hid_status] = useState(false);
-    const [dcs_bios_status, set_dcs_bios_status] = useState(false);
+    const [pfp_hid_status, set_pfp_hid_status] = useState("❔");
+    const [ufc_hid_status, set_ufc_hid_status] = useState("❔");
+    const [dcs_bios_status, set_dcs_bios_status] = useState("❔");
     const [dcs_module, set_dcs_module] = useState("");
     const [writer_stdout, set_writer_stdout] = useState([[""], [""]]);
 
     async function start(program) {
+        if (program == "pfp_writer") set_pfp_hid_status("❔");
+        else if (program == "ufc_writer") set_ufc_hid_status("❔");
+
+        set_dcs_bios_status("❔");
         await invoke("start", { program });
     }
 
     async function set_stdout_data() {
         let out = await invoke("read_available", {});
+        // assume DCS Bios is ✅ and replace it with ❌
+        // if something goes wrong
+        set_dcs_module("To be implemented");
         if (out[0].length > 0 || out[1].length > 0) {
             for (let i = 0; i <= 1; i++) {
                 let latest_line = String(out[i]);
                 // PFP HID FALSE
-                if (latest_line.startsWith("Error: PFP_WRITER: No HID")) {
-                    set_pfp_hid_status(false);
+                if (latest_line.includes("Error: PFP_WRITER: No HID")) {
+                    set_pfp_hid_status("❌");
                 }
                 // UFC HID FALSE
-                else if (latest_line.startsWith("Error: UFC_WRITER: No HID")) {
-                    set_ufc_hid_status(false);
+                else if (latest_line.includes("Error: UFC_WRITER: No HID")) {
+                    set_ufc_hid_status("❌");
                 }
                 // PFP HID TRUE
                 else if (
-                    latest_line.startsWith("PFP_WRITER: Sent init packets")
+                    latest_line.includes("PFP_WRITER: Sent init packets")
                 ) {
-                    set_pfp_hid_status(true);
+                    set_pfp_hid_status("✅");
                 }
                 // UFC HID TRUE
-                else if (latest_line.startsWith("UFC_WRITER: HID device OK")) {
-                    set_ufc_hid_status(true);
+                else if (latest_line.includes("UFC_WRITER: HID device OK")) {
+                    set_ufc_hid_status("✅");
                 }
                 // DCS BIOS FALSE
-                else if (latest_line.startsWith("read_stream error")) {
-                    set_dcs_bios_status(false);
+                else if (latest_line.includes("read_stream error")) {
+                    set_dcs_bios_status("❌");
+                }
+                // DCS BIOS FALSE
+                else if (latest_line.includes("Connection closed")) {
+                    set_dcs_bios_status("❌");
                 }
                 // DCS BIOS TRUE
-                else if (latest_line.startsWith("Module")) {
-                    set_dcs_bios_status(true);
-                    set_dcs_module("To be implemented");
+                if (latest_line.includes("Connection OK")) {
+                    console.log("A");
+                    set_dcs_bios_status("✅");
                 }
             }
-            let append_condition = String(out).startsWith("Module:");
-            // console.log(append_condition);
-            if (!append_condition) {
-                set_writer_stdout((pfp_writer_stdout) => [
-                    ...pfp_writer_stdout,
-                    out,
-                ]);
-            }
+            // if () {
+            set_writer_stdout((pfp_writer_stdout) => [
+                ...pfp_writer_stdout,
+                out,
+            ]);
+            // }
         }
+    }
+
+    function clear_console() {
+        set_writer_stdout(() => []);
     }
 
     useEffect(() => {
@@ -73,25 +86,29 @@ function App() {
                     <h1>PFP Writer</h1>
                     <button onClick={() => start("pfp_writer")}>⏻</button>
                     <div className="status_panel">
-                        <p>HID: {`${pfp_hid_status ? "✅" : "❌"}`}</p>
-                        <p>DCS Bios: {`${dcs_bios_status ? "✅" : "❌"}`}</p>
+                        <p>Device connection:{pfp_hid_status}</p>
+                        <p>DCS Bios: {dcs_bios_status}</p>
                     </div>
                 </div>
                 <div className="program_wrapper">
                     <h1>UFC Writer</h1>
                     <button onClick={() => start("ufc_writer")}>⏻</button>
                     <div className="status_panel">
-                        <p>HID: {`${ufc_hid_status ? "✅" : "❌"}`}</p>
-                        <p>DCS Bios: {`${dcs_bios_status ? "✅" : "❌"}`}</p>
+                        <p>Device connection:{ufc_hid_status}</p>
+                        <p>DCS Bios: {dcs_bios_status}</p>
                     </div>
                 </div>
             </div>
-            <div className="debug_console">
-                {" "}
-                <p>.</p>
-                {writer_stdout?.map((entry, i) => (
-                    <div key={"stout_" + i}>{entry}</div>
-                ))}
+            <div className="debug_console_container">
+                <div className="debug_console">
+                    {writer_stdout?.map((entry, i) => (
+                        <div key={"stout_" + i}>{entry}</div>
+                    ))}
+                    <p>.</p>
+                </div>
+                <button onClick={() => clear_console()}>
+                    MASTER<br></br>CAUTION
+                </button>
             </div>
         </>
     );
