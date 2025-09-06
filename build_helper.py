ad8747f1eb82c2a2aebdf9bbdb6fd46c99940e30
Daniel <74920220+Kurisu003@users.noreply.github.com>
2025-09-07 00:48:37 +0200

    Automate build and packaging in build_helper.py
    Added steps to build UWACS, move executables and dependencies to custom_out, and create main_archive.zip. Updated .gitignore to exclude custom_out and main_archive.zip.

--- build_helper.py
+++ build_helper.py
@@ -34,3 +34,53 @@ shutil.copy2(
     r"C:\Users\danie\Documents\GitHub\ufc_writer [important]\rust\target\release\ufc_writer.exe",
     r"C:\Users\danie\Documents\GitHub\UWACS\children"
 )
+
+# Build UWACS
+subprocess.run(
+    "npm run tauri build",
+    cwd=r"C:\Users\danie\Documents\GitHub\UWACS",
+    shell=True
+)
+
+# Move UWACS Exe to folder
+shutil.copy2(
+    r"C:\Users\danie\Documents\GitHub\UWACS\src-tauri\target\release\uwacs.exe",
+    r"C:\Users\danie\Documents\GitHub\UWACS\custom_out\Main"
+)
+
+# Move Deps folder
+shutil.copytree(
+    r"C:\Users\danie\Documents\GitHub\UWACS\src-tauri\target\release\deps",
+    r"C:\Users\danie\Documents\GitHub\UWACS\custom_out\Main\deps",
+    dirs_exist_ok=True  # available in Python 3.8+
+)
+
+# Move Resources folder
+shutil.copytree(
+    r"C:\Users\danie\Documents\GitHub\UWACS\src-tauri\target\release\resources",
+    r"C:\Users\danie\Documents\GitHub\UWACS\custom_out\Main\resources",
+    dirs_exist_ok=True  # available in Python 3.8+
+)
+
+# Move output4
+shutil.copy2(
+    r"C:\Users\danie\Documents\GitHub\PFP 3N DCS Bios [important]\target\release\output4.txt",
+    r"C:\Users\danie\Documents\GitHub\UWACS\custom_out\Main"
+)
+
+# Move PFP exe
+shutil.copy2(
+    r"C:\Users\danie\Documents\GitHub\PFP 3N DCS Bios [important]\target\release\pfp_writer.exe",
+    r"C:\Users\danie\Documents\GitHub\UWACS\custom_out\children"
+)
+
+# Move UFC exe
+shutil.copy2(
+    r"C:\Users\danie\Documents\GitHub\ufc_writer [important]\rust\target\release\ufc_writer.exe",
+    r"C:\Users\danie\Documents\GitHub\UWACS\custom_out\children"
+)
+
+
+folder_to_zip = r"C:\Users\danie\Documents\GitHub\UWACS\custom_out"
+output_zip = r"C:\Users\danie\Documents\GitHub\UWACS\main_archive"
+shutil.make_archive(output_zip, 'zip', folder_to_zip)
