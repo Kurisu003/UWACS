5dfae66c9b19cce575fd716eb0a75d40bcd918b3
Daniel <74920220+Kurisu003@users.noreply.github.com>
2025-09-07 10:28:19 +0200

    Add leader line image and update main.rs for path
    Added AH-64D_MFCD_leaderLine.png to the children directory. Updated main.rs to construct the path to this image using the current directory. Cargo.lock changes reflect dependency updates.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{env, path::PathBuf};
+
 
 fn main() {
+    let mut current_dir: PathBuf = env::current_dir().unwrap();
+    current_dir.pop();
+    current_dir.push("children");
+    current_dir = current_dir.join("AH-64D_MFCD_leaderLine.png");
+
     uwacs_lib::run()
 }
